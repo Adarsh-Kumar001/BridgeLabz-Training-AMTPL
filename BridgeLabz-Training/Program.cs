6b633ef71f28edf3d3009127be57ae6481f3a26d
Adarsh <adarshkumar102004@gmail.com>
2025-10-25 14:25:09 +0530

    rock paper scissor game

--- BridgeLabz-Training/Program.cs
+++ BridgeLabz-Training/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+public class RockPaperScissor
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the name of moves of the two players separated by a space: ");
+        String inpt = Console.ReadLine();
+        String[] moves = inpt.Split(' ');
+
+        if (moves.Length == 2)
+        {
+            if (moves[0].ToLower() == "rock")
+            {
+                if (moves[1].ToLower() == "paper")
+                {
+                    Console.WriteLine("Player 2 wins");
+                }
+                else if (moves[1].ToLower() == "scissor")
+                {
+                    Console.WriteLine("Player 1 wins");
+                }
+            }
+            else if (moves[0].ToLower() == "paper")
+            {
+                if (moves[1].ToLower() == "rock")
+                {
+                    Console.WriteLine("Player 1 wins");
+                }
+                else if (moves[1].ToLower() == "scissor")
+                {
+                    Console.WriteLine("Player 2 wins");
+                }
+            }
+            else if (moves[0].ToLower() == "scissor")
+            {
+                if (moves[1].ToLower() == "rock")
+                {
+                    Console.WriteLine("Player 2 wins");
+                }
+                else if (moves[1].ToLower() == "paper")
+                {
+                    Console.WriteLine("Player 1 wins");
+                }
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Invalid number of players, this game needs exactly two players!");
+        }
+    }
+
+}
--- BridgeLabz-Training/obj/Release/net8.0/BridgeLabz-Training.AssemblyInfo.cs
+++ BridgeLabz-Training/obj/Release/net8.0/BridgeLabz-Training.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BridgeLabz-Training")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+006060634641f580e09bbcfddc39088d72a82c5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BridgeLabz-Training")]
