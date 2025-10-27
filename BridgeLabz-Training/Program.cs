bb8869af86c2bcd47be38e066d05224cdca973aa
Adarsh <adarshkumar102004@gmail.com>
2025-10-27 17:06:55 +0530

    oops

--- BridgeLabz-Training/Program.cs
+++ BridgeLabz-Training/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Main Dev Branch");
+﻿using System;
+
+class Animal 
+{
+
+    public int NumberOfLegs;
+    public string Name;
+    public string Colour;
+
+    public Animal(int n, string name, string colour) {
+
+        this.NumberOfLegs = n;
+        this.Name = name;
+        this.Colour = colour;
+    
+    }
+
+    public virtual void getAnimalData() {
+
+        Console.WriteLine($"The animal is: {Name} and it has {NumberOfLegs} legs and it's {Colour} in colour!");
+
+    }
+
+
+}
+
+
+class OOPS
+{
+
+
+    static void Main(string[] args)
+    {
+        Animal a1 = new Animal(4, "dog", "black");
+
+        a1.getAnimalData();
+
+        Console.ReadLine();
+
+    }
+}
