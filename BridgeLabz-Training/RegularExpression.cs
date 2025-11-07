b4f717b8674246cee975520e1e5795f571daaa84
Adarsh <adarshkumar102004@gmail.com>
2025-11-07 17:11:46 +0530

    regex

---
+++ BridgeLabz-Training/RegularExpression.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class RegularExpression
+{
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a phone number: ");
+        string phoneNumber  = Console.ReadLine();
+
+        string strRegex = @"(^[0-9]{10}$)|(^\+[0-9]{2}\s+[0-9]
+                {2}[0-9]{8}$)|(^[0-9]{2}-[0-9]{10}$)";
+
+        Regex rg = new Regex(strRegex);
+
+        if (rg.IsMatch(phoneNumber))
+        {
+            Console.WriteLine("Its a valid phone number");
+        }
+        else
+        {
+            Console.WriteLine("Its not a valid phone number");
+        }
+
+    }
+}
