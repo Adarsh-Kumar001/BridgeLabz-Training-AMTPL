6a01b286630217e05481a65c14e683e277d7836f
Adarsh <adarshkumar102004@gmail.com>
2025-11-08 10:57:46 +0530

    queue demo

---
+++ BridgeLabz-Training/collections/QueueDemo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz_Training.collections
+{
+    internal class QueueDemo
+    {
+        public static void Main(string[] args)
+        {
+            Queue<string> q1 = new Queue<string>();
+
+            q1.Enqueue("1");       //Adds element at last..1
+            q1.Enqueue("2");      //Adds element at last...1,2
+            q1.Enqueue("3");     //Adds element at last....1,2,3
+
+            Console.WriteLine(q1.Dequeue()); //Removes and returns the element as start...=> 1
+
+        }
+    }
+}
