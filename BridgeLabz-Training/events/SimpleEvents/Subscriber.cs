c6cd585eca44c3e87f7cbd6f7197673a0961f593
Adarsh <adarshkumar102004@gmail.com>
2025-12-16 16:00:29 +0530

    events

--- BridgeLabz-Training/events/SimpleEvents/Subscriber.cs
+++ BridgeLabz-Training/events/SimpleEvents/Subscriber.cs
@@ -6,8 +6,8 @@ using System.Threading.Tasks;
 
 namespace BridgeLabz_Training.events.SimpleEvents
 {
-    internal class Subscriber         //classes that recieve or handle the event //subscribers cant raise the event, only publisher can. hence p.processcompleted
-    {
+    internal class Subscriber           //classes that recieve or handle the event //subscribers cant raise the event, only publisher can. hence p.processcompleted
+    {                                  //subscribers recieve event when the event has occured. Publsihers handles the event and when its occured subscribers get info
         public void Handle(string str)         //matches delegate signature 
         { 
             Console.WriteLine("Message recieved" + str);
